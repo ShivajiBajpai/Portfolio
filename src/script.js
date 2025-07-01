fa4c3caf7ad4d9a4344280069084a038b7cea3c7
Shivaji Bajpai <shivajisjbbajpai@gmail.com>
2025-07-01 23:26:59 +0530

    Improve focus order on menu bar

--- src/script.js
+++ src/script.js
@@ -6,6 +6,19 @@ console.log(navigationMenuButton);
 navigationMenuButton.addEventListener("click", ()=>{
     const isExpanded = navigationMenuButton.getAttribute('aria-expanded') === 'true';
     navigationMenuButton.setAttribute('aria-expanded', !isExpanded);
-
+    
     navPanel.classList.toggle("hidden");
+    if(!isExpanded){
+        const firstfocusableelement = document.querySelector('a[href="#aboutMe"]');
+        if(firstfocusableelement){
+            firstfocusableelement.focus();
+        }
+    }
+})
+
+
+document.addEventListener('focusin', (e)=>{
+    if(!(navPanel.contains(e.target)) && e.target !== navigationMenuButton){
+        navPanel.classList.add("hidden");
+    }
 })
